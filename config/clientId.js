bf3a98d14a57bf317066b67a8301857db4e0b992
yonureker <yonureker@gmail.com>
2020-01-02 15:46:52 -0800

    make keys public for demo

---
+++ config/clientId.js
@@ -0,0 +1,4 @@
+export default clientId = {
+  iosClientId: "691486029945-ab0tvpd5mcc9kej5s6u8ctip8jv0br5j.apps.googleusercontent.com",
+  androidClientId: "691486029945-m1m7fm641el96u6de6kmsbcfk81kqrt8.apps.googleusercontent.com",
+}
